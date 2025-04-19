b00adf627895b31c6699bcb57bbb11c583e73c46
Diogo Bettencourt <diogobettencourt97@gmail.com>
2025-04-19 12:04:57 +0100

    Update: MessageDTO

--- src/test/java/org/api/automation/foundation/model/MessageDTO.java
+++ src/test/java/org/api/automation/foundation/model/MessageDTO.java
@@ -3,6 +3,8 @@ package org.api.automation.foundation.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class MessageDTO {
 
@@ -11,4 +13,20 @@ public class MessageDTO {
 
     @JsonProperty("message")
     private String message;
+
+    @JsonProperty("errors")
+    private List<ErrorDTO> errors;
+
+    @Data
+    private static class ErrorDTO {
+
+        @JsonProperty("path")
+        private String path;
+
+        @JsonProperty("message")
+        private String message;
+
+        @JsonProperty("errorCode")
+        private String errorCode;
+    }
 }
